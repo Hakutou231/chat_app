c9b293e40e985a576df24665c2e42730e08647d2
anhpdhleon <62009325+anhpdhleon@users.noreply.github.com>
2021-10-21 15:54:40 +0900

    <Update>: Pick Image for Avatar

--- lib/widgets/auth_form_widget.dart
+++ lib/widgets/auth_form_widget.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import '../widgets/pickers/user_image_picker.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -22,10 +25,25 @@ class _AuthFormState extends State<AuthForm> {
   var _userEmail = '';
   var _userName = '';
   var _userPass = '';
+  File? _userImageFile;
+
+  void _pickedImage(File image) {
+    _userImageFile = image;
+  }
 
   void _trySubmit() {
     final isValid = _formKey.currentState!.validate();
     FocusScope.of(context).unfocus();
+
+    if (_userImageFile == null) {
+      Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Please pick an image'),
+          backgroundColor: Theme.of(context).errorColor,
+        ),
+      );
+      return;
+    }
 
     if (isValid) {
       _formKey.currentState!.save();
@@ -52,6 +70,7 @@ class _AuthFormState extends State<AuthForm> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
+                  if (!_isLogin) UserImagePicker(_pickedImage),
                   TextFormField(
                     key: ValueKey('email'),
                     validator: (value) {
@@ -101,12 +120,12 @@ class _AuthFormState extends State<AuthForm> {
                   ),
                   if (widget.isLoading) CircularProgressIndicator(),
                   if (!widget.isLoading)
-                  RaisedButton(
-                    onPressed: () {
-                      _trySubmit();
-                    },
-                    child: Text(_isLogin ? 'Login' : 'Signup'),
-                  ),
+                    RaisedButton(
+                      onPressed: () {
+                        _trySubmit();
+                      },
+                      child: Text(_isLogin ? 'Login' : 'Signup'),
+                    ),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
                     onPressed: () {
---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+
+  final void Function(File pcikedImage) imagePickFn;
+
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImage;
+
+  void _pickImage() async {
+    final pickedImageFile = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+    widget.imagePickFn(pickedImageFile);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          backgroundImage: FileImage(_pickedImage!),
+        ),
+        FlatButton.icon(
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+          textColor: Theme.of(context).primaryColor,
+        ),
+      ],
+    );
+  }
+}
